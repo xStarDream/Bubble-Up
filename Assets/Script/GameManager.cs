271d6f3fa5e55ce5b8d1bf10537df88dd49b4881
xStarDream <s.marcotripoli.94@gmail.com>
2025-01-24 21:28:19 +0100

    .

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Ciao");
+
     }
 
     // Update is called once per frame
