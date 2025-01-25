377fd621f5db5fced83485420545859ef345b0bf
marcobon90 <marcus.form.90@outlook.it>
2025-01-25 10:01:20 +0100

    - Aggiunto prefab Barra. Creare la gestione delle due barre

--- Assets/Script/hit-bar/Moving Bar/CenterSquare.cs
+++ Assets/Script/hit-bar/Moving Bar/CenterSquare.cs
@@ -1,26 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class HitBar : MonoBehaviour
+public class CenterSquare : MonoBehaviour
 {
-
-
-    // [SerializeField] Bar bar;
-
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
-
 }
