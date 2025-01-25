49368e40fedee161128cd258dfccb59d2177d102
Giovanni Pittera <55334969+Kuroshiroi@users.noreply.github.com>
2025-01-25 13:21:48 +0100

    GameManager first atempt
    Gamemanager / Storyboard Scene set Up

---
+++ Assets/Script/Scene_Controller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public static class Scene_Controller 
+{
+    public static void LoadScene(int sceneIndex)
+    {
+        LoadScene(sceneIndex);
+    }
+    public static void RestartGame()
+    {
+        LoadScene(0);
+    }
+
+    public static void RestartLevel()
+    {
+        LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public static void NextLevel()
+    {
+        if(SceneManager.sceneCountInBuildSettings>SceneManager.GetActiveScene().buildIndex+1)
+            LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
