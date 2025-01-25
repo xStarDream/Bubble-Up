1f07bfdb6153f17a82c427f14f84c146c131d55f
xStarDream <s.marcotripoli.94@gmail.com>
2025-01-25 16:41:56 +0100

    Bugfix restart

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject canva;
+
     public void RestartLevelGameManager()
     {
         Scene_Controller.RestartLevel();
+        canva.SetActive(true);
+
     }
 }
